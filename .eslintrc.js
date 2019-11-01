e8a2122925d299fe1918ffd24eb8648932d0ba82
xTCry <wolt6@mail.ru>
2019-11-02 00:48:52 +0300

    eslnt upd

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,30 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
+		"@typescript-eslint/interface-name-prefix": ["error", "always"],
+		"@typescript-eslint/no-non-null-assertion": "off",
+		"@typescript-eslint/indent": ["error", "tab"],
+		"@typescript-eslint/camelcase": ["error", {
+			"properties": "never"
+		}],
+		"@typescript-eslint/ban-ts-ignore": "off",
+		"@typescript-eslint/semi": ["error"],
+		"import/no-cycle": "off",
+		"no-restricted-syntax": ["error", "WithStatement"],
+		"comma-dangle": ["error", "never"],
+		"no-param-reassign": ["error", {
+			"props": false
+		}],
+		"no-dupe-class-members": "off",
+		"no-await-in-loop": "off",
+		"arrow-parens": "off",
+		"no-continue": "off",
+		"no-tabs": ["error", {
+			"allowIndentationTabs": true
+		}],
+		"indent": "off",
+		"semi": "off",
+		"linebreak-style": ["error", "windows"]
     },
 	"overrides": [
 		{
